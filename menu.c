2a579ee6eeb78731f8fa2946c2171ed907b9d29e
Jouueur <95230708+Jouueur@users.noreply.github.com>
2022-06-05 14:15:48 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -6,13 +6,25 @@
 #include "board_display.h"
 
 
+
 void print_first_menu(int board[ROWS][COLS]){ 
     // print menu and start game if 1 is pressed
     int choice;
 
-    printf("************************************\n");
+    printf("\033[34;01m************************************\n");
     printf("               TETRIS\n\n  (1) Jouer\n  (2) Quitter\n\n");
-    printf("************************************\n");
+    printf("************************************\033[00m\n");
+
+    do{
+        scanf("%d",&choice);
+    }while((choice != 1) && (choice != 2));
+
+    if(choice == 1)start_game(board);
+    else{
+        exit(1);
+    }
+
+}
 
     do{
         scanf("%d",&choice);
